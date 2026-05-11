b24fe935d3dd7b3062d74020d144bdfb3e84e1d3
lourencoborgessilva <lourencoborgessilva@gmail.com>
2026-05-10 23:55:32 -0300

    estou arrumando algumas coisas

--- Sprint1/Simulador_de_Sessão_de_Recarga/script.js
+++ Sprint1/Simulador_de_Sessão_de_Recarga/script.js
@@ -1,35 +1,72 @@
-///Construir um programa que simule:
+                                    ///Construir um programa que simule:
 
-//●	Início e fim de sessão de recarga
-//●	Controle básico de energia
-//●	Registro de dados da sessão
-//●	Aplicação de regras simples de cobrança
+                                    //●	Início e fim de sessão de recarga
+                                    //●	Controle básico de energia
+                                    //●	Registro de dados da sessão
+                                    //●	Aplicação de regras simples de cobrança
 
 
 let energiaInicial = 18;
-
 document.getElementById("porcentagem-bateria").innerText = energiaInicial + "%";//peguei o elemento do html e atribui a ele o valor da energia inicial para mostrar a porcentagem da bateria na tela
 
 let carregando;
 let energiaFinal = 100;
 let energiaAtual = energiaInicial;
 let custoPorCarregamento = 0.50;// custo da recarga
+
+
+
+//um comando universal para nao repetir em certas partes
+//ele chama um id do html e adiciona no javascript
+function chamarBotao(idBotao, evento, funcao) {
+    let botao = document.getElementById(idBotao);
+
+    botao.addEventListener(evento, funcao);
+}
+
+
+
 let tempoDeRecarga = 0;
+function atualizarTempo() {
+    tempoDeRecarga += 1;
+
+    document.getElementById("tempo-decorrido").innerText =
+        `${tempoDeRecarga}s`;
+}
 
 
-let botao = document.getElementById("btn-continuar-sessao");
+
+//função para calcular o custo da recarga
+function custoPelaRecarga() {
+
+    let energiaCarregada = energiaInicial;
+
+    let custoTotal = energiaCarregada * custoPorCarregamento;
+
+    document.getElementById("custo-estimado").innerText =
+        `R$ ${custoTotal.toFixed(2)}`;
+}
+
+
+
+chamarBotao("btn-continuar-sessao", "click", iniciarRecarga);
 //função para iniciar a recarga
 function iniciarRecarga() {
     if (!carregando) {//verifica se já existe um processo de recarga em andamento
-//simula o processo de recarga aumentando a energia a cada segundo
+                      //simula o processo de recarga aumentando a energia a cada segundo
         carregando = setInterval(() => {
 
             if (energiaInicial < 100) {
                 energiaInicial += 10;
-                tempoDeRecarga += 1;
+                
 
                 document.getElementById("porcentagem-bateria").innerText =
                     energiaInicial + "%";
+
+                    atualizarTempo();           //atualiza em tempo real o tempo da recarga
+
+                    custoPelaRecarga();         //atualiza em tempo real o quanto vai custar o carregamento
+
             } else {
 
                 clearInterval(carregando);
@@ -42,12 +79,11 @@ function iniciarRecarga() {
         }, 1000);
     }
 }
-botao.addEventListener("click", iniciarRecarga);
 
 
 
+chamarBotao("btn-pausar-sessao", "click", pararRecarga);
 //função para pausar a recarga
-let botaoParar = document.getElementById("btn-pausar-sessao");
 function pararRecarga() {
 
     clearInterval(carregando);
@@ -56,7 +92,6 @@ function pararRecarga() {
 
     console.log("Recarga pausada");
 }
-botaoParar.addEventListener("click", pararRecarga);
 
 
 
@@ -71,11 +106,4 @@ function finalizarRecarga() {
 
 
 
-//função para calcular o custo da recarga
-let botaoFinalizar = document.getElementById("btn-finalizar-sessao");
-function calcularCusto() {
-    let energiaCarregada = energiaFinal - energiaInicial;
-    let custoTotal = energiaCarregada * custoPorCarregamento;
-    alert(`O custo total da recarga é: R$ ${custoTotal.toFixed(2)}`);
-}
-botaoFinalizar.addEventListener("click", calcularCusto);
+
